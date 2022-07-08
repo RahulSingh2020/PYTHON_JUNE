fb981e5c657c04c5e738706e447df96715309e3a
RahulSingh2020 <108917539+RahulSingh2020@users.noreply.github.com>
2022-07-08 11:03:29 +0530

    Writing First Python Code !!!

---
+++ L1.py
@@ -0,0 +1,51 @@
+
+#################################################
+# Day 02  : 27.06.2022
+#################################################
+#################################################
+# 23.06.2022
+# Writing First Python Code !!!
+#################################################
+# topics to be covered
+# sHORCOMINGS IN pYTHON
+#  Interpreting language vs compiling
+# intro to official website and docs for reference
+# current python version
+# printing Hello World
+
+
+
+#################################################
+
+# sHORCOMINGS IN pYTHON 
+# Slow Speed
+    # Interpreting language/
+    # runtime error (not compilation error)
+    # Interpreting language vs compiling 
+
+# Interpreting language
+    # code is executed line by line -- run 
+    # first line 
+    # second line 
+    # slow
+
+# compiling 
+    # you write a code 
+    # bundle it 
+    # compile -- run 
+    # eg 300 lines of code
+    # fast 
+
+# Interpreting language vs compiling
+# ordering food at restaurant is like Interpreting
+# At home | complete thali | compiling
+
+
+# not memory efficient 
+# Weak in mobile computing , used in server side only 
+
+# beginner friendly 
+#################################################
+# intro to official website and docs for reference
+# NOTE : while installing , check  the "add to path" option
+# vs code setting 
